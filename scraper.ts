21efd6f0a010e23cd812c8e4f91d94c019ca8f77
simone <simonedisante@gmail.com>
2019-10-27 11:57:39 +0100

    fix

--- scraper.ts
+++ scraper.ts
@@ -2,6 +2,7 @@ import * as puppeteer from "puppeteer"
 import * as request from 'request'
 import * as DomParser from 'dom-parser';
 
+
 export class Scraper {
 
     browser;
@@ -43,49 +44,58 @@ export class Scraper {
         })
 
     }
+    request(){
+
+        const parser = new DomParser();
+        request('http://www.diretta.it', function (error, response, body) {
+
+        });
+    }
 
     async scrap(fn: (arg)=> any){
         const browser = await puppeteer.launch({});
         const page = await browser.newPage();
         await page.goto('https://www.diretta.it', {waitUntil: 'networkidle2'});
 
+        console.log("icasdf")
+        let results = [] ;
 
         setInterval(async ()=>{
-            const result = await page.evaluate( () => {
+
+            const x = await page.evaluate( () => {
+                console.log("icasdf")
 
                 let arr = [];
                 document.querySelectorAll(".event__match").forEach( (match: any) => {
                     const home = match.querySelector(".event__participant--home").innerText;
                     const away = match.querySelector(".event__participant--away").innerText;
                     const results = match.querySelector(".event__scores").querySelectorAll('span');
-                    arr.push(home + (results[0] ? ' ' + results[0].innerText : '') + " - "+ (results[1] ?  results[1].innerText + ' ' : '') + away);
 
                     arr.push({
                         home: {
-                            home,
-                            score: results[0]
+                            team: home,
+                            score: results[0] ? ' ' + results[0].innerText : ''
                         },
                         away:{
-                            away,
-                            score: results[1]
-                        }
+                            team: away,
+                            score: results[1] ? ' ' + results[1].innerText : ''                        }
                     })
 
+                    console.log(arr);
+
                 })
-                return arr;
+                return JSON.stringify(arr);
             })
 
-            fn(result);
+            fn(JSON.parse(x))
 
         }, 1000)
 
     }
 }
 
-/*
 new Scraper().init("https://www.diretta.it").then(re => {
-    re.scrap((v)=>{
-        console.log(new Date())
+    re.scrap((results)=>{
+        console.log(results, new Date());
     }).then();
 });
-*/
