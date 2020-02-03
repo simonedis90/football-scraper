3b1537696a733666f1446e558c528f645d961d4a
simone <simonedisante@gmail.com>
2020-02-04 00:36:31 +0100

    some fixes

--- scraper.ts
+++ scraper.ts
@@ -4,98 +4,63 @@ import * as DomParser from 'dom-parser';
 
 
 export class Scraper {
-
-    browser;
-    page;
-    constructor(){
+    constructor() {
 
     }
 
-    init(page): Promise<Scraper>{
+    init(): Promise<Scraper> {
         return new Promise((resolve) => {
             puppeteer.launch({}).then(browser => {
-                this.browser = browser;
-                this.browser.newPage().then(page => {
-                    this.page = page;
-
-                    page.goto('http://www.diretta.it', {waitUntil: 'networkidle2'}).then(re => {
-                        resolve(this);
-                    });
-                })
+                resolve(this);
             })
         })
 
     }
 
-    evaluate(page): Promise<Array<any>>{
-        return new Promise((resolve) => {
-            page.evaluate( eva => {
-
-                const arr = [];
-                document.querySelectorAll(".event__match").forEach( (match: any) => {
-                    const home = match.querySelector(".event__participant--home").innerText;
-                    const away = match.querySelector(".event__participant--away").innerText;
-                    const results = match.querySelector(".event__scores").querySelectorAll('span');
-                    arr.push(home + (results[0] ? ' ' + results[0].innerText : '') + " - "+ (results[1] ?  results[1].innerText + ' ' : '') + away);
-                })
-
-                resolve(arr);
-            })
-        })
-
-    }
-    request(){
-
-        const parser = new DomParser();
-        request('http://www.diretta.it', function (error, response, body) {
-
-        });
-    }
-
-    async scrap(fn: (arg)=> any){
+    async scrap(fn: (arg) => any, pageUrl, interval?) {
         const browser = await puppeteer.launch({});
         const page = await browser.newPage();
-        await page.goto('https://www.diretta.it', {waitUntil: 'networkidle2'});
+        await page.goto(pageUrl, { waitUntil: 'networkidle2' });
 
-        console.log("icasdf")
-        let results = [] ;
-
-        setInterval(async ()=>{
-
-            const x = await page.evaluate( () => {
-                console.log("icasdf")
-
+        const fns = async () => {
+            const x = await page.evaluate(() => {
                 let arr = [];
-                document.querySelectorAll(".event__match").forEach( (match: any) => {
+
+                document.querySelectorAll(".event__match").forEach((match: any) => {
+
                     const home = match.querySelector(".event__participant--home").innerText;
                     const away = match.querySelector(".event__participant--away").innerText;
                     const results = match.querySelector(".event__scores").querySelectorAll('span');
-
                     arr.push({
                         home: {
                             team: home,
-                            score: results[0] ? ' ' + results[0].innerText : ''
+                            score: results[0] ? parseInt(results[0].innerText) : 0
                         },
-                        away:{
+                        away: {
                             team: away,
-                            score: results[1] ? ' ' + results[1].innerText : ''                        }
+                            score: results[1] ? parseInt(results[1].innerText) : 0
+                        }
                     })
-
-                    console.log(arr);
-
                 })
                 return JSON.stringify(arr);
             })
 
             fn(JSON.parse(x))
 
-        }, 1000)
-
+        }
+        if (interval) {
+            setInterval(async () => {
+                fns()
+            }, 1000)
+        } else {
+            fns();
+        }
     }
 }
 
-new Scraper().init("https://www.diretta.it").then(re => {
-    re.scrap((results)=>{
-        console.log(results, new Date());
-    }).then();
-});
+// new Scraper().init().then(re => {
+//     re.scrap((results) => {
+//         console.log(results, new Date());
+//         process.abort();
+//     }, "https://www.diretta.it").then();
+// });
